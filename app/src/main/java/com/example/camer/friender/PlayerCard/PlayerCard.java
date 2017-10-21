59f0739f476a180c3da38377ad07e4bfdf4b46f7
Sheldon <shl322@ucsd.edu>
2017-10-21 16:52:39 -0700

    Added drawable files

---
+++ app/src/main/java/com/example/camer/friender/PlayerCard/PlayerCard.java
@@ -0,0 +1,15 @@
+package com.example.camer.friender.PlayerCard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.camer.friender.R;
+
+public class PlayerCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_card);
+    }
+}
