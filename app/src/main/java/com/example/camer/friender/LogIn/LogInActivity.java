a95ad0ffae60f6d95f4d61fd5de5e797f9fe4ebd
Stephanie <stepht750@gmail.com>
2017-10-21 16:54:35 -0700

    Added Log in screen

---
+++ app/src/main/java/com/example/camer/friender/LogIn/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.example.camer.friender.LogIn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.camer.friender.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
