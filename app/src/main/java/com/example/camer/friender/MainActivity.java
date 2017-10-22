713389b03af34de4ea006f256d6fa5ddacfc011b
Sheldon <shl322@ucsd.edu>
2017-10-21 18:05:03 -0700

    Aligned player card images

--- app/src/main/java/com/example/camer/friender/MainActivity.java
+++ app/src/main/java/com/example/camer/friender/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // testing
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_player_card);
     }
 }
